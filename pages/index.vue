b79b445267ac327b365212b9556ed0d2f9fdf6a9
Garnet Crookesgit@ssh.dev.azure.com:v3/childfriendlycare/Child%20Friendly/web-core-vite <crookes.garnet@gmail.com>
2023-12-29 20:03:20 -0700

    Add name with animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
 <template>
     <div>
-        Home
+        <div class="bg-slate-900/25 p-4 mt-8 border-none">
+            <div class="text-4xl text-bold py-2">
+                Hi, I'm
+            </div>
+            <div class="container relative-position">
+                <div class="aboslute top-0 animate-in text-6xl text-bold text-primary py-2">
+                    Garnet Crookes
+                </div>
+            </div>
+            <div class="text-4xl text-bold py-2">
+                Full Stack Developer
+            </div>
+        </div>
     </div>
 </template>
 
+<style lang="sass">
+.animate-in
+    transform: translateY(100%)
+    animation: slideInUp 5s forwards
+
+.container
+    overflow: hidden
+
+@keyframes slideInUp
+  to
+    opacity: 1
+    transform: translateY(0)
+</style>
