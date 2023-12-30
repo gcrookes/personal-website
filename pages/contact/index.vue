42485825605dde354f487a2311d0cc3b6897d68d
Garnet Crookesgit@ssh.dev.azure.com:v3/childfriendlycare/Child%20Friendly/web-core-vite <crookes.garnet@gmail.com>
2023-12-30 00:17:29 -0700

    Add a contact page frontend. Still need a database

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>Contact Page</div>
+    <div class="row q-col-gutter-x-sm q-gutter-y-md">
+        <BorderedBox
+            class="col-12 q-ml-xs"
+            title="Contact"
+            :body="headerMessage"
+            key="title"
+        />
+        <q-input
+            label="Name"
+            class="col-12 col-md-6"
+            v-model="message.name"
+            outlined
+            dark
+       />
+        <q-input
+            label="Email Address"
+            class="col-12 col-md-6"
+            type="email"
+            v-model="message.email"
+            outlined
+            dark
+        />
+        <q-input
+            label="Message"
+            class="col-12"
+            type="textarea"
+            v-model="message.message"
+            outlined
+            dark
+        />
+        <q-btn
+            label="Submit"
+            color="white"
+            class="ml-auto mr-2"
+            size="lg"
+            dense
+            outline
+        />
+    </div>
 </template>
+
+
+<script setup lang="ts">
+    const message = ref({email: '', name: '', message: ''})
+const headerMessage = 'Reach out for any inquires and I will get back to you promptly'
+</script>
