5ead83a4f373fca0b8def9f23d231c5188792006
Garnet Crookesgit@ssh.dev.azure.com:v3/childfriendlycare/Child%20Friendly/web-core-vite <crookes.garnet@gmail.com>
2024-01-06 15:43:43 -0700

    Add github and youtube links

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -23,6 +23,18 @@
                     </a>
                 </div>
             </div>
+            <div class="row q-gutter-x-sm absolute -top-4 right-4">
+                <a v-if="info.youtube" :href="info.youtube" target="_blank">
+                    <div class="bg-zinc-800 border-white border-2 content-center row rounded-lg h-8 w-8 p-0.5">
+                        <img src="/logos/youtube.png" title="Github" alt="Link to demo on Youtube" class="max-h-full max-w-full">
+                    </div>
+                </a>
+                <a v-if="info.github" :href="info.github" target="_blank">
+                    <div class="bg-zinc-800 border-white border-2 rounded-lg h-8 w-8 p-0.5">
+                        <img src="/logos/github-mark-white.png" title="Github" alt="Link to source code on Github" class="max-h-full max-w-full">
+                    </div>
+                </a>
+            </div>
         </BorderedBox>
     </div>
 </template>
@@ -34,20 +46,6 @@
       "These are some of the projects I have been working on lately",
 }
 const projects = [
-  {
-    title: "Ticket System",
-    body: "A sample website with a system to purchase tickets for a movie theater chain (Cineplex clone)",
-    bullets: [
-        "Frontend implemented using React, users can login or use the site as a guest",
-        "Backend implemented in Spring Boot, with data stored on a MySQL database",
-    ],
-    logos: [
-        { name: "javascript", link: "https://www.javascript.com/" },
-        { name: "react", link: "https://react.dev/" },
-        { name: "springboot", link: "https://spring.io/projects/spring-boot/" },
-        { name: "mysql", link: "https://www.mysql.com/" },
-    ],
-  },
   {
     title: "EOG Mouse Control",
     body: "A microproccesor based system to control a computer mouse using brainwaves. For the NatHacks Hackathon",
@@ -62,6 +60,23 @@ const projects = [
         { name: "arduino", link: "https://www.arduino.cc/" },
         { name: "exg", link: "https://store.upsidedownlabs.tech/product/bioamp-exg-pill/" },
     ],
+    github: "https://github.com/gcrookes/EOGMouseControl",
+    youtube: "https://www.youtube.com/watch?v=8hNw2gWGpAQ&ab_channel=MohammedAlhajjaj",
+  },
+  {
+    title: "Ticket System",
+    body: "A sample website with a system to purchase tickets for a movie theater chain (Cineplex clone)",
+    bullets: [
+        "Frontend implemented using React, users can login or use the site as a guest",
+        "Backend implemented in Spring Boot, with data stored on a MySQL database",
+    ],
+    logos: [
+        { name: "javascript", link: "https://www.javascript.com/" },
+        { name: "react", link: "https://react.dev/" },
+        { name: "springboot", link: "https://spring.io/projects/spring-boot/" },
+        { name: "mysql", link: "https://www.mysql.com/" },
+    ],
+    github: "https://github.com/ChengDave/ticket-reservation-system",
   },
 ];
 </script>
